0ebd4e762f88ccc2f287b117ce8f349afe6ceab6
Mtan1230 <m.tan1230@gmail.com>
2023-06-12 17:06:24 -0400

    encapsulate function generatLogo() and generate example

--- index.js
+++ index.js
@@ -1,33 +1,9 @@
 const { askQuestions } = require('./lib/prompt');
-const {Circle, Square, Triangle} = require('./lib/shapes');
-const fs = require('fs');
-
-function generateLogo(text, textColor, shape, shapeColor) {
-    let logo;
-    switch (shape) {
-        case 'circle':
-            logo = new Circle(text, textColor, shapeColor);
-            break;
-        case 'square': 
-            logo = new Square(text, textColor, shapeColor);
-            break;
-        default:
-            logo = new Triangle(text, textColor, shapeColor);
-    }
-
-    const content = `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-    ${logo.renderShape()}
-    ${logo.renderText()}
-    </svg>`;
-
-    const fileName = `./examples/${text}-${shape}.svg`
-    fs.writeFile(fileName, content, (err) => 
-        err ? console.log(err) : console.log('Generated logo.svg'))
-}
+const { generateLogo } = require('./lib/generateLogo');
 
 async function init() {
     const data = await askQuestions();
-    const {text, textColor, shape, shapeColor} = data;
+    const { text, textColor, shape, shapeColor } = data;
     generateLogo(text, textColor, shape, shapeColor);
 }
 init()
